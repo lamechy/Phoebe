0bea715922805285edfb65559a1306db97ba391f
lamechy <lamechongondi88@gmail.com>
2020-11-23 12:15:30 +0300

    add the updated main.js file

--- js/main.js
+++ js/main.js
@@ -225,7 +225,7 @@ jQuery(document).ready(function($) {
       var hash = this.hash;
 
       $('html, body').animate({
-        'scrollTop': $(hash).offset().top - 0
+        'scrollTop': $(hash).offset().top-0
       }, 1000, 'easeInOutCirc', function(){
         window.location.hash = hash;
       });
